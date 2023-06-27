cee6ca02bf18be7f0a06b171e5c8eb8dcc76e5d3
Radu Boian <boianradu@gmail.com>
2023-06-27 22:19:53 +0200

    feat: array groups exercise

---
+++ reverse_array_in_groups/raig.py
@@ -0,0 +1,21 @@
+#User function template for Python
+
+class Solution:	
+    #Function to reverse every sub-array group of size k.
+  def reverseInGroups(self, arr, N, K):
+        print("ana are mere")
+        for x in arr:
+            print(x)
+    # code here
+ 
+import math
+def main():
+    N = 5
+    K = 3
+    arr = {1,2,3,4,5}
+    ob = Solution()
+    ob.reverseInGroups(arr,N,K)
+
+if __name__=="__main__":
+    main()
+ 
